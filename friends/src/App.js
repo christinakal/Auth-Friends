36a2c9867ae14a19d86b6da3483320beb5021cce
christina kaliora <develop@pirron.one>
2020-01-29 14:58:13 +0100

    main functionality in place == some styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from './components/Auth/PrivateRoute';
 import "./App.css";
-import Login from "./components/Login";
+import Login from "./components/Auth/Login";
+import Friends from './components/Friends'
+import AddFriend from './components/AddFriend'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />
+        <Route exact path="/login" component={Login} />
+        <PrivateRoute path="/friends" component={Friends} />
+        <PrivateRoute path="/add-friend" component={AddFriend} />
       </header>
-    </div>
+    </div >
   );
 }
 
---
+++ friends/src/components/FriendsCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './FriendsCard.css';
+
+const FriendsCard = props => {
+    console.log(props);
+    return (
+        <div className='card'>
+            <h3>{props.name}</h3>
+            <p>{props.age}</p>
+            <p>{props.email}</p>
+        </div>
+    )
+}
+
+export default FriendsCard;
